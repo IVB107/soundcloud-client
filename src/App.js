0d76830073944bc006f07a62ea3a56e38b317e77
IVB107 <ianvb107@gmail.com>
2019-09-06 17:38:54 -0500

    Added component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from 'react'
+import Styled from 'styled-components'
+
+import Nav from './components/Nav'
+import Search from './components/Search'
+import Player from './components/Player'
+import Filter from './components/Filter'
+import Results from './components/Results'
 
 function App() {
   return (
     <div>
-
+      <Nav />
+      <Search />
+      <Results />
+      <Filter />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default Nav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
