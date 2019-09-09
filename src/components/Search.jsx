0ed6e8ea6d1a43352c204869f411dc1fbf18a2f6
IVB107 <ianvb107@gmail.com>
2019-09-09 15:30:36 -0500

    Added structure/styling to TrackList component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Styled from 'styled-components'
-// import SearchResult from './SearchResult'
+import SearchResult from './SearchResult'
 
 const Search = () => {
   return (
     <SearchContainer>
       <form>
         <input type="text" placeholder="Search by artist"/>
+        <div>
+        <p>Artist</p>
+        <p>|</p>
+        <p>Track</p>
+        </div>
       </form>
       <ResultsContainer >
         {/* Map over selected artists & render <SearchResult /> for each */}
+        <SearchResult />
       </ResultsContainer>
     </SearchContainer>
   )
@@ -27,6 +33,7 @@ const SearchContainer = Styled.div`
   form {
     display: flex;
     flex-grow: 1;
+    align-items: center;
     border-bottom: 1px solid #eaf1f7;
     margin: 0 20px;
 
@@ -34,9 +41,11 @@ const SearchContainer = Styled.div`
       flex-grow: 1;
       border: none;
       background: none;
-      padding: 20px 0 6px;
+      /* padding: 20px 0 6px; */
+      padding: 6px 0;
       font-size: 1.4rem;
       color: #eaf1f7;
+      height: 1.4rem;
     }
 
     input::placeholder {
@@ -44,11 +53,23 @@ const SearchContainer = Styled.div`
       font-weight: 500;
       color: rgba(234, 241, 247, .7);
     }
+
+    div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0;
+      margin: 0;
+
+      p {
+        font-size: .8rem;
+        margin: 0 4px;
+      }
+    }
   }
 `
 
 const ResultsContainer = Styled.div`
-  border: 2px solid black;
   display: flex;
   align-items: center;
   justify-content: flex-start;
--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
+import Styled from 'styled-components'
 
 const TrackList = () => {
   return (
-    <div>
-      
-    </div>
+    <TrackContainer>
+      <ListHeader>
+        <h2>Tracklist</h2>
+        <div>
+          <p># of tracks</p>
+          <p>45:17</p>
+        </div>
+      </ListHeader>
+      <ListHeader>
+        <p>Sort By:</p>
+        <div>
+          <button>DANCEABILITY</button>
+          <button>ENERGY</button>
+          <button>TEMPO</button>
+        </div>
+      </ListHeader>
+    </TrackContainer>
   )
 }
 
 export default TrackList
+
+const TrackContainer = Styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 70vw;
+  margin: 0 20px;
+  align-self: flex-start;
+`
+
+const ListHeader = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 4px 0;
+
+  h2 {
+    margin: 0;
+    color: #eaf1f7;
+  }
+
+  p {
+    margin: 0;
+    color: #eaf1f7;
+    opacity: .7;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    p {
+      margin: 0 0 0 10px;
+      font-size: .8rem;
+      color: #eaf1f7;
+      opacity: .7;
+    }
+
+    button {
+      border: 1px solid white;
+      background: none;
+      color: white;
+      margin: 0 0 0 10px;
+      padding: 4px 6px;
+    }
+  }
+`
