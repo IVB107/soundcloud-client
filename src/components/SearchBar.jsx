e50ae3b5c68fc41dc226de16cc0e5849b2f6d9ae
IVB107 <ianvb107@gmail.com>
2019-09-18 18:08:05 -0500

    Added dropdown for API search results

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useEffect } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import Styled from 'styled-components'
 import Spotify from 'spotify-web-api-js'
 
@@ -8,7 +8,7 @@ import { searchReducer } from '../reducers/searchReducer'
 const spotifyApi = new Spotify()
 
 const SearchBar = () => {
-  const [keyword, setKeyword] = useState('')
+  // const [keyword, setKeyword] = useState('')
 
   const [search, dispatch] = useReducer(searchReducer, {
     searchType: ['artist'],
@@ -25,7 +25,6 @@ const SearchBar = () => {
       results: []
     })
     if (input !== ''){
-      console.log('Searching...')
       await spotifyApi.search(input, search.searchType)
         .then(response => {
           console.log('Input Search Results: ', response)
@@ -33,7 +32,7 @@ const SearchBar = () => {
             type: 'ON_CHANGE',
             searchType: search.searchType,
             input: input,
-            results: response.artists.items.splice(0, 5)
+            results: search.searchType[0] === 'artist' ? response.artists.items.splice(0, 5) : response.tracks.items.splice(0, 5)
           })
         })
         .catch(err => {
@@ -48,8 +47,8 @@ const SearchBar = () => {
       dispatch({
         type: 'SWITCH_TYPE',
         searchType: [type],
-        input: search.input,
-        results: search.results
+        input: '',
+        results: []
       })
     }
   }
@@ -61,9 +60,13 @@ const SearchBar = () => {
 
   return (
     <SearchContainer>
-      <form>
-      <input type="text" value={search.input} onChange={e => inputSearch(e.target.value)} placeholder={`Search by ${search.searchType[0]}`}/>
-        {/* <input type="text" value={keyword} onChange={e => setKeyword(e.target.value)} placeholder="Search by artist"/> */}
+      <form onSubmit={null}>
+      <input 
+        type="text" 
+        value={search.input} 
+        onChange={e => inputSearch(e.target.value)} 
+        placeholder={`Search by ${search.searchType[0]}`}
+      />
         <div>
           <SearchType 
             style={search.searchType[0] === 'artist' ? {color: `#eaf1f7`} : null} 
@@ -80,6 +83,17 @@ const SearchBar = () => {
           </SearchType>
         </div>
       </form>
+      <ResultOptions>
+        { search.results.length > 0 && (
+          search.results.map(item => (
+            <li>
+              <img src={search.searchType[0] === 'artist' ? item.images[0].url : item.album.images[0].url} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))
+        )
+        }
+      </ResultOptions>
       <ResultsContainer >
         {/* Map over selected artists & render <SearchResult /> for each */}
         <SearchResult />
@@ -127,17 +141,14 @@ const SearchContainer = Styled.div`
       align-items: center;
       padding: 0;
       margin: 0;
-
-      p {
-        font-size: .8rem;
-        margin: 0 4px;
-      }
     }
   }
 `
 
 const SearchType = Styled.p`
   cursor: pointer;
+  font-size: .8rem;
+  margin: 0 4px;
   /* color: #eaf1f7;  */
 `
 
@@ -148,4 +159,40 @@ const ResultsContainer = Styled.div`
   margin: 0 20px;
   padding: .5rem 0;
   min-height: 3rem;
+`
+
+const ResultOptions = Styled.ul`
+  height: 20px;
+  /* width: 100%; */
+  padding: 0;
+  margin: 0 20px;
+  z-index: 10;
+  list-style-type: none;
+
+  li {
+    height: 36px;
+    background-color: rgba(18, 38, 45);
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin: none;
+    padding: 0 10px;
+    list-style-type: none;
+
+    img, p {
+      padding: 0 10px;
+    }
+
+    img {
+      height: 30px;
+      width: 30px;
+      border-radius: 50%;
+    }
+
+    p {
+      color: #eaf1f7;
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+  }
 `
