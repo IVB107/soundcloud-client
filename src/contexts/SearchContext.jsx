5faff633f521a3f3817c4b2b626bceb8b9497625
IVB107 <ianvb107@gmail.com>
2019-09-19 18:52:44 -0500

    Added context provider for search results

---
+++ src/contexts/SearchContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useReducer } from 'react'
+import { searchReducer } from '../reducers/searchReducer'
+
+export const SearchContext = createContext()
+
+const SearchContextProvider = (props) => {
+  // Set initial authentication state
+  const [search, dispatch] = useReducer(searchReducer, {
+    searchType: ['artist'],
+    input: '',
+    results: []
+  })
+  return (
+    <SearchContext.Provider value={{search, dispatch}}>
+      {props.children}
+    </SearchContext.Provider>
+  )
+}
+
+export default SearchContextProvider
