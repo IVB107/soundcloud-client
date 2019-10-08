b3ebb58fd1b018f81003e6c95079df51419a60c7
IVB107 <ianvb107@gmail.com>
2019-10-08 13:38:08 -0500

    Built out logic to successfully pass search filters with API request

--- src/components/OptionType.jsx
+++ src/components/OptionType.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react'
+import Spotify from 'spotify-web-api-js'
 import Styled from 'styled-components'
 import { Slider } from '@material-ui/core'
 
@@ -6,10 +7,25 @@ import { SearchContext } from '../contexts/SearchContext'
 
 const OptionType = ({ option }) => {
   const { search, dispatch } = useContext(SearchContext)
+  const spotifyApi = new Spotify()
+  let options = {
+    min_popularity: search.options.popularity[0],
+    max_popularity: search.options.popularity[1],
+    min_energy: parseFloat(search.options.energy[0])/100,
+    max_energy: parseFloat(search.options.energy[1])/100,
+    min_instrumentalness: parseFloat(search.options.vocals[0])/100,
+    max_instrumentalness: parseFloat(search.options.vocals[1])/100,
+    min_tempo: parseInt(search.options.tempo[0]*1.84) + 24,
+    max_tempo: parseInt(search.options.tempo[1]*1.84) + 24,
+    min_danceability: parseFloat(search.options.danceability[0])/100,
+    max_danceability: parseFloat(search.options.danceability[1])/100,
+    min_valence: parseFloat(search.options.mood[0])/100,
+    max_valence: parseFloat(search.options.mood[1])/100,
+    min_acousticness: parseFloat(search.options.acoustics[0])/100,
+    max_acousticness: parseFloat(search.options.acoustics[1])/100,
+  }
 
   const handleChange = (event, newValue) => {
-    console.log('Option Change EVENT: ', event)
-    console.log('Option Change NEW VALUE: ', newValue)
     dispatch({
       type: 'UPDATE_OPTIONS',
       ...search,
@@ -20,17 +36,37 @@ const OptionType = ({ option }) => {
     })
   }
 
-  useEffect(() => {
-    // Find a way to call getRecommendations() from SelectionContainer.jsx
-    // Use setTimeout to wait until slider action is complete before making API call
-  }, [search.options])
+  const updateRecommendations = async () => {
+    console.log('OPTIONS OBJECT: ', options)
+    await spotifyApi.getRecommendations(
+      search.searchType[0] === 'artist'
+        ? {seed_artists: search.selected.map(selection => selection.id), ...options} // search by artist (default)
+        : {seed_tracks: search.selected.map(selection => selection.id), ...options} // search by track
+      )
+      .then(response => {
+        console.log('Recommendations: ', response)
+        dispatch({
+          type: 'UPDATE_SUGGESTED_TRACKS',
+          ...search,
+          suggested_tracks: response.tracks,
+          current_track: response.tracks[0]
+        })
+      })
+      .catch(err => {
+        console.log('ERROR: ', err)
+      })
+  }
+
+  const sliderCommitted = () => {
+    updateRecommendations()
+  }
 
   return (
     <OptionContainer>
       <p>{option.toUpperCase()}</p>
       <Slider
         value={search.options[option]}
-        // onMouseDownCapture={handleChange}
+        onChangeCommitted={sliderCommitted}
         onChange={handleChange}
         color='secondary'
         // valueLabelDisplay="auto"
--- src/components/SelectionContainer.jsx
+++ src/components/SelectionContainer.jsx
@@ -8,29 +8,13 @@ import SelectedItem from './SelectedItem'
 const SelectionContainer = () => {
   const { search, dispatch } = useContext(SearchContext)
   const spotifyApi = new Spotify()
-  let options = {
-    min_popularity: search.options.popularity[0],
-    max_popularity: search.options.popularity[1],
-    min_energy: search.options.energy[0],
-    max_energy: search.options.energy[1],
-    min_speechiness: search.options.vocals[0],
-    max_speechiness: search.options.vocals[1],
-    min_tempo: search.options.tempo[0],
-    max_tempo: search.options.tempo[1],
-    min_danceability: search.options.danceability[0],
-    max_danceability: search.options.danceability[1],
-    min_valence: search.options.mood[0],
-    max_valence: search.options.mood[1],
-    min_acousticness: search.options.acoustics[0],
-    max_acousticness: search.options.acoustics[1],
-  }
   
   const getRecommendations = async () => {
     if (search.selected.length > 0){
       await spotifyApi.getRecommendations(
         search.searchType[0] === 'artist'
-          ? {seed_artists: search.selected.map(selection => selection.id), ...options} // search by artist (default)
-          : {seed_tracks: search.selected.map(selection => selection.id), ...options} // search by track
+          ? {seed_artists: search.selected.map(selection => selection.id)} // search by artist (default)
+          : {seed_tracks: search.selected.map(selection => selection.id)} // search by track
         )
         .then(response => {
           console.log('Recommendations: ', response)
